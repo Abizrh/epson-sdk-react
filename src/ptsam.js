5a780c9da91c7ce5cf3c0411424676f2e7a69c11
Abizrh <abizarah857@gmail.com>
2023-03-28 21:54:19 +0700

    fix (master/ptsam): resolve layout and functionality

--- src/ptsam.js
+++ src/ptsam.js
@@ -3,69 +3,101 @@ import numberToWords from "./utils/format";
 
 let encoder = new EscPosEncoder();
 
-const price = 90_000
+const price = 90_000;
+const ex = [
+  {
+    id: 1,
+    name: "Test Darah",
+  },
+  {
+    id: 2,
+    name: "Swab",
+  },
+  {
+    id: 3,
+    name: "Antigen",
+  },
+  {
+    id: 4,
+    name: "Vaksin",
+  },
+  {
+    id: 5,
+    name: "Baru",
+  },
+  {
+    id: 6,
+    name: "Test",
+  },
+];
+
+const MAX_COLS = 4; // Maksimal jumlah kolom per baris
+
+// Membuat array 2D untuk menyimpan hasil konversi data ex
+const result = [[]];
+
+// Menggunakan loop untuk mengubah data ex
+ex.forEach((item, index) => {
+  // Jika jumlah kolom pada baris terakhir sudah mencapai maksimum, maka tambahkan array baru untuk menampung data selanjutnya
+  if (result[result.length - 1].length >= MAX_COLS) {
+    result.push(['', '', '', '']);
+
+    // Jika jumlah kolom pada baris pertama sudah mencapai maksimum, tambahkan array kosong setelah baris pertama
+    if (result.length === 2 && result[0].length === MAX_COLS) {
+      result.push([]);
+    }
+  }
+
+  // Menambahkan item name ke dalam array result
+  result[result.length - 1].push(item.name);
+});
+
 
 encoder
   .initialize()
   .size("medium")
   .align("center")
-  // .line("_".repeat(80))
-  // .newline()
+  .newline()
+  .newline()
+  .newline()
   .table(
     [
       { width: 45, align: "right" },
       { width: 10, align: "left" },
-      { width: 25, align: "left" },
+      { width: 20, align: "right" },
     ],
     [
-      [``, `092323`, "            10/20/2023"],
-      [``, `Abizarah`, ""],
-      [``, `22th 1bl 2hr`, ""],
+      ["", `092323`, "10/20/2023"],
+      ["", `Abizarah`, ""],
+      ["", "22th 1bl", ""],
       ["", "Azure", ""],
-      ["", "Abizarah", "Hal 1 / 1"],
+      ["", `Abizarah`, "Hal 1 / 1"],
     ]
   )
-  // .line("_".repeat(80))
-  //* header
+  .newline()
+  .newline()
+  .newline()
   .table(
     [
-      { width: 10, align: "left" },
-      { width: 55, align: "left" },
-      { width: 15, align: "right" },
+      { width: 20, align: "left" },
+      { width: 20, align: "left" },
+      { width: 20, align: "left" },
+      { width: 20, align: "left" },
     ],
-    [
-      ["", "", ""],
-    ]
+    result
   )
-  // .line("_".repeat(80))
-  .table([{ width: 70, align: "left" }], [
-    // ["JENIS PEMERIKSAAN"]
-  ])
   .newline()
-  .table([{ width: 75, align: "left" }], [
-    ["SARS CoV 1 Swab Antigen"],
-    [""],
-    ["Darah Lengkap"],
-    [""],
-    ["Analisa Feses (METODE FLOTASI)"],
-    [""],
-  ])
   .newline()
-  //* footer
-  // .line("_".repeat(80))
+  .newline()
+  .newline()
+  .newline()
   .table(
     [
       { width: 60, align: "left" },
-      { width: 10, align: "left" },
+      { width: 15, align: "left" },
     ],
-    [
-      [
-        "",
-        `Rp. ${String(price)}`
-      ],
-    ]
+    [["", `Rp. ${String(price)}`]]
   )
-  .newline()
   .newline()
   .table(
     [
@@ -73,20 +105,24 @@ encoder
       { width: 55, align: "left" },
       { width: 15, align: "left" },
     ],
-    [["", `  ${numberToWords(price)}`, ""]]
+    [["", `${numberToWords(price)}`, ""]]
   )
-  // .line("_".repeat(80))
+  .newline()
+  .newline()
+  .newline()
+  .newline()
   .table(
     [
       { width: 50, align: "left" },
       { width: 30, align: "left" },
     ],
     [
-      // ["", "ULTRA MEDICA"]
+      [
+        "",
+        "Qin Shi Huang .mdK"
+      ],
     ]
   )
-  .newline()
-  .newline()
 
 // add newLine to push the paper out
 for (let i = 0; i < 35; i++) {
